ff8b7a5b3e5b2c0cde05b13181991ad2cbc297bd
Pavel Koryakin <unclekoin@gmail.com>
2022-09-15 13:27:47 +0300

    feat: add product component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { IProduct } from './models/product';
+import { products as data} from './data/products';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-app';
+  title = 'online store';
+  products: IProduct[] = data;
 }
