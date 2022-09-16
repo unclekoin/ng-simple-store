e1378cd852cc5d39775262364432c6a9111078e6
Pavel Koryakin <unclekoin@gmail.com>
2022-09-16 15:20:37 +0300

    feat: add GlobalError & services

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,32 @@
-import { Component } from '@angular/core';
+import { Observable, tap } from 'rxjs';
+import { Component, OnInit } from '@angular/core';
 import { IProduct } from './models/product';
-import { products as data} from './data/products';
+import { ProductService } from './services/product.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'online store';
-  products: IProduct[] = data;
+  // products: IProduct[] = [];
+  products$: Observable<IProduct[]>;
+  loading = false;
+
+  constructor(private productService: ProductService) {}
+
+  ngOnInit(): void {
+    this.loading = true;
+    this.products$ = this.productService.getAll()
+      .pipe(
+        tap(() => this.loading = false)
+      )
+    // this.productService.getAll().subscribe({
+    //   next: (products: IProduct[]) => this.products = products,
+    //   complete: () => {
+    //     this.loading = false;
+    //   }
+    // })
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ProductComponent } from './components/product.component';
+import { ProductComponent } from './components/product/product.component';
+import { HttpClientModule } from '@angular/common/http';
+import { GlobalErrorComponent } from './components/global-error/global-error.component';
 
 @NgModule({
-  declarations: [AppComponent, ProductComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [AppComponent, ProductComponent, GlobalErrorComponent],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
