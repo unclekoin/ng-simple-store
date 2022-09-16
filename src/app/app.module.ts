c7cc2a44b8d44c2ccbe59fe2f6f8c38f9c77d2e4
Pavel Koryakin <unclekoin@gmail.com>
2022-09-16 17:21:19 +0300

    feat: add CreateProductComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,8 +6,11 @@ import { AppComponent } from './app.component';
 import { ProductComponent } from './components/product/product.component';
 import { HttpClientModule } from '@angular/common/http';
 import { GlobalErrorComponent } from './components/global-error/global-error.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { FilterProductsPipe } from './pipes/filter-products.pipe';
+import { ModalComponent } from './components/modal/modal.component';
+import { CreateProductComponent } from './components/create-product/create-product.component';
+import { FocusDirective } from './directives/focus.directive';
 
 @NgModule({
   declarations: [
@@ -15,8 +18,17 @@ import { FilterProductsPipe } from './pipes/filter-products.pipe';
     ProductComponent,
     GlobalErrorComponent,
     FilterProductsPipe,
+    ModalComponent,
+    CreateProductComponent,
+    FocusDirective,
   ],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
